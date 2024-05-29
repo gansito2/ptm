458e74d01436d4b4a20bca75907316677bc3d9b4
Adrian Cancino <71229190+Adrian-Cancino@users.noreply.github.com>
2024-05-28 20:19:07 -0600

    Update settings.py

--- p001crud/settings.py
+++ p001crud/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&gyr5%6)d#mmsf2*u5gj*g05!_1z$tzmaf)gr%vg6@nf7xz=s0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.136.97.1', 'localhost']
+ALLOWED_HOSTS = [3.144.219.198', 'localhost']
 
 
 # Application definition
